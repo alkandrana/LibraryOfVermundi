44f4baf16fd040bc55ae76ca5e6ecb82791c2a39
Rosa Myers <larosaardente@gmail.com>
2024-11-22 11:41:11 -0800

    Completed setup for generating the database.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using LibraryOfVermundi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryOfVermundi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    
+    public DbSet<Entry> Entries { get; set; }
+    
+    public DbSet<AppUser> AppUsers { get; set; }
+}
